03bb5fd658d2709edfe961bd70da529152dac936
Alexandre Kilian <alexandre@pulpmedia.at>
2014-10-01 11:02:11 +0200

    added sunblocks back in

--- Resources/public/scripts/controllers/block-element-options.js
+++ Resources/public/scripts/controllers/block-element-options.js
@@ -20,15 +20,15 @@ angular.module('angularApp')
     }
 
   $scope.ok = function () {
-    // if($scope.childType=='widget'){
+    if($scope.childType=='widget'){
         Block.addWidget({id:block.id,type:$scope.widgetType.id}).$promise.then(function(widget){
             $modalInstance.close(widget);
         });
-    // } else {
-    //     Block.addBlock({id:block.id,name:$scope.blockName}).$promise.then(function(subblock){
-    //         $modalInstance.close(subblock);
-    //     });
-    // }
+    } else {
+        Block.addBlock({id:block.id,name:$scope.blockName}).$promise.then(function(subblock){
+            $modalInstance.close(subblock);
+        });
+    }
   };
 
   $scope.cancel = function () {
